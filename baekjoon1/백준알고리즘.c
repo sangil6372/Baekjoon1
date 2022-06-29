16a031592ea30431f0c1ba3bd73c0d981eb07fbf
Sang il Park <sangil6372@naver.com>
2022-06-29 13:18:10 +0900

    도전

--- baekjoon1/백준알고리즘.c
+++ baekjoon1/백준알고리즘.c
@@ -3,34 +3,24 @@
 #pragma warning(disable : 4996)
 
 
+int FirstNum_Of_Line(int n) {
+	return 3 * n*(n + 1) + 1;
+}
+
 int main() {
-	
-	double arr[1000] = { 0 };
-	int rep;
-	double max = 0;
-	int max_index = 0;
-	double result = 0;
 
-	scanf("%d", &rep);
+	int x;
+	scanf("%d", &x);
 
-
-	for (int i = 0; i < rep; i++) {
-		scanf("%lf", &arr[i]);
-	}
-
-	for (int i = 0; i < rep; i++) {
-		if (arr[i] > max) {
-			max_index = i;
-			max = arr[i];
+	int line = 0;
+	while (1) {
+		if (FirstNum_Of_Line(line) > x) {
+			line = line - 1;
+			break;
 		}
+		line++;
 	}
+	printf("%d", line);
 
-	for (int i = 0; i < rep; i++) {
-			result += arr[i] / max * 100;	
-	}
-	
-	printf("%.14lf", result / rep);
-	
-
-
+	return 0;
 }
